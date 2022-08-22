52eaa5610511ec709e926edb5d2eac13ab05d91f
ARm8-2 <77163307+ARm8-2@users.noreply.github.com>
2022-08-22 12:13:14 +0200

    added url for config

--- packages/oslo-vue-frontend/src/components/SelectComponent.vue
+++ packages/oslo-vue-frontend/src/components/SelectComponent.vue
@@ -57,7 +57,7 @@ export default {
     }
   },
   async mounted() {
-    const response = await fetch('https://raw.githubusercontent.com/Informatievlaanderen/OSLO-Validator-EU/dev/packages/oslo-itb-rdf-validator/resources/applicatieprofielen/config.properties');
+    const response = await fetch(process.env.CONFIGURATION_URL);
     const data = await response.text(); //get github config.properties content as text
 
     var obj = {};
